f6b6ccf6a0fe39dc35989108b209b88eafa47d26
Herbert Araújo <herbertaraujo.contact@gmail.com>
2023-07-17 08:58:53 -0300

    feat: NextAuth config

--- src/app/api/auth/[...nextauth].ts
+++ src/app/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
+import bcrypt from 'bcrypt';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { AuthOptions } from 'next-auth';
+import NextAuth, { AuthOptions } from 'next-auth';
 import prisma from '@/prismadb';
 
 import GithubProvider from 'next-auth/providers/github';
@@ -17,6 +18,43 @@ export const authOptions: AuthOptions = {
       clientId: process.env.GOOGLE_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
     }),
-    // CrendentialsProvider({}),
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'email', type: 'text' },
+        password: { label: 'password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials?.password)
+          throw new Error('Invalid credentials!');
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email },
+        });
+
+        if (!user || !user?.hashedPassword) {
+          throw new Error('Invalid credentials!');
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          user?.hashedPassword,
+          credentials.password
+        );
+
+        if (!isCorrectPassword) throw new Error('Invalid credentials!');
+
+        return user;
+      },
+    }),
   ],
+  pages: {
+    signIn: '/',
+  },
+  debug: process.env.NODE_ENV === 'development',
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 };
+
+export default NextAuth(authOptions);
